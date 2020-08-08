c864e93542512878540ed8c2541a8b6302e38475
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-08-09 02:32:38 +0530

    edits-2:32pm

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import Ninjas from './Ninjas';
 
 function App() {
+
+  const ninjas = [
+    {name:'Nishant', age:'21', belt:'black', id:1},
+    {name:'Amit', age:'20', belt:'red', id:2},
+    {name:'Aditya', age:'20', belt:'brown', id:3}
+  ];
+  
   return (
     <div className="App">
       <h1>First React App</h1>
-      <Ninjas name="Amit" age="20" belt="black" />
+      <Ninjas ninjas = {ninjas} />
     </div>
   );
 }
