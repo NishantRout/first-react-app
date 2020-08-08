9a90a287fc74401537516cf91cc4662ed2c70d2c
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-08-09 01:27:38 +0530

    edits-1:27pm

---
+++ src/Ninjas.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Ninjas extends Component{
+    render(){
+        return(
+            <div class="ninja">
+                <div>Name: Nishant</div>
+                <div>Age: 21</div>
+                <div>Belt: Black</div>
+            </div>
+        )
+    }
+}
+
+export default Ninjas;
