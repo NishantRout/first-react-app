09dbfdbbc07a17c5cc3855d25004e437f5f04d7c
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-08-09 16:45:38 +0530

    edits-4:45pm

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import Ninjas from './Ninjas';
 
-function App() {
-
-  const ninjas = [
-    {name:'Nishant', age:'21', belt:'black', id:1},
-    {name:'Amit', age:'20', belt:'red', id:2},
-    {name:'Aditya', age:'20', belt:'brown', id:3}
-  ];
-  
-  return (
-    <div className="App">
-      <h1>First React App</h1>
-      <Ninjas ninjas = {ninjas} />
-    </div>
-  );
+class App extends Component {
+  state={
+    ninjas : [
+      {name:'Nishant', age:'21', belt:'black', id:1},
+      {name:'Amit', age:'20', belt:'red', id:2},
+      {name:'Aditya', age:'20', belt:'brown', id:3}
+    ]
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1>First React App</h1>
+        <Ninjas ninjas = {this.state.ninjas} />
+      </div>
+    );
+  }
 }
 
 export default App;
